0de02c69255bbb8287506f20bfa87379bf9a737e
RC <riley.c.gamer@gmail.com>
2023-02-13 17:41:48 -0500

    Got buttons to change

--- SimonSays/GameScreen.cs
+++ SimonSays/GameScreen.cs
@@ -14,9 +14,11 @@ namespace SimonSays
 {
     public partial class GameScreen : UserControl
     {
-       
+
 
         int guess = 0; //Get guesses
+
+
         public GameScreen()
         {
             InitializeComponent();
@@ -24,7 +26,7 @@ namespace SimonSays
 
         private void GameScreen_Load(object sender, EventArgs e)
         {
-            
+
             Form1.pattern.Clear();
             this.Refresh();
             Thread.Sleep(1000);
@@ -35,11 +37,24 @@ namespace SimonSays
         {
             //TODO: get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list. Each number represents a button. For example, 0 may be green, 1 may be blue, etc.
             Random random = new Random();
-            Form1.pattern.Add(random.Next(0, 4));
+            Form1.pattern.Add(1);
 
             //TODO: create a for loop that shows each value in the pattern by lighting up approriate button
+            for (int i = 0; i < Form1.pattern.Count; i++)
+            {
+                if (Form1.pattern[i] == 1)
+                {
+                   
+                    greenButton.BackColor = Color.White;
+                    this.Refresh();
+                    Thread.Sleep(900);
+                    greenButton.BackColor = Color.ForestGreen;
+                  
+                    
+                }
+            }
 
-
+            guess = 0;
             //TODO: set guess value back to 0
         }
 
@@ -47,16 +62,16 @@ namespace SimonSays
         private void greenButton_Click(object sender, EventArgs e)
         {
             //TODO: is the value in the pattern list at index [guess] equal to a green?
-                // change button color
-                // play sound
-                // refresh
-                // pause
-                // set button colour back to original
-                // add one to the guess variable
-             
+            // change button color
+            // play sound
+            // refresh
+            // pause
+            // set button colour back to original
+            // add one to the guess variable
+
             //TODO:check to see if we are at the end of the pattern, (guess is the same as pattern list count).
-                // call ComputerTurn() method
-                // else call GameOver method
+            // call ComputerTurn() method
+            // else call GameOver method
         }
 
         public void GameOver()
